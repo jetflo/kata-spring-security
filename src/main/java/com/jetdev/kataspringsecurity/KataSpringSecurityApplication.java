7483b8675a1e8c47e4cb9acc9bc0e5c8a924d219
Florian Lemaire <florian.lemaire.gis@gmail.com>
2023-09-15 09:08:20 +0200

    :tada: initialize project

---
+++ src/main/java/com/jetdev/kataspringsecurity/KataSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jetdev.kataspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jetdev/kataspringsecurity/KataSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetdev.kataspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
